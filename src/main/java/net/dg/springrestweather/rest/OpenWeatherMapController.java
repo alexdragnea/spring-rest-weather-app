1ee518d09bcc8d1b7c5ee021602e7d31a624bbcd
Alexandru Dragnea <alexdg722@gmail.com>
2022-11-20 23:21:30 +0200

    refactor

--- src/main/java/net/dg/springrestweather/rest/OpenWeatherMapController.java
+++ src/main/java/net/dg/springrestweather/rest/OpenWeatherMapController.java
@@ -5,12 +5,10 @@ import net.dg.springrestweather.model.WeatherData;
 import net.dg.springrestweather.service.impl.OpenWeatherMapServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api")
