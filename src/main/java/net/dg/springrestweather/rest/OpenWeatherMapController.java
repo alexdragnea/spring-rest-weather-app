bab3a49c6a6437b17958b49a692e328c2f87b00d
Alexandru Dragnea <alexdg722@gmail.com>
2022-11-22 20:17:36 +0200

    refactor code

--- src/main/java/net/dg/springrestweather/rest/OpenWeatherMapController.java
+++ src/main/java/net/dg/springrestweather/rest/OpenWeatherMapController.java
@@ -3,8 +3,6 @@ package net.dg.springrestweather.rest;
 import lombok.AllArgsConstructor;
 import net.dg.springrestweather.model.WeatherData;
 import net.dg.springrestweather.service.impl.OpenWeatherMapServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class OpenWeatherMapController {
 
-  private static final Logger logger = LoggerFactory.getLogger(OpenWeatherMapController.class);
   private final OpenWeatherMapServiceImpl openWeatherMapService;
 
   @GetMapping(value = "/current")
@@ -27,9 +24,6 @@ public class OpenWeatherMapController {
 
   @GetMapping
   public ResponseEntity<WeatherData> getWeatherBasedOnCity(@RequestParam String city) {
-    logger.info(
-        "Inside getWeatherBasedOnCity method from OpenWeatherMapController, asking temperature for [city = {}]",
-        city);
 
     return ResponseEntity.ok(openWeatherMapService.getWeatherByCity(city));
   }
